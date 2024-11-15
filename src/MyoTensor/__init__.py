71149e926f636ed35b9ce3d2401b1cd65356b554
josephbrunet <jo.brunet73@gmail.com>
2024-11-15 22:35:00 +0100

    repackage

--- src/MyoTensor/__init__.py
+++ src/MyoTensor/__init__.py
@@ -1,10 +1,5 @@
 from MyoTensor import (  # noqa
     processing_heart,
     processing_functions,
-    analysis_functions,
     utils,
-    gui,
-    slurm_launcher,
-    create_vtk_file,
-    create_amira_file,
 )
