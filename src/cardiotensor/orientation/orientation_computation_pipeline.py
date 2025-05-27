d19b89f7055c72bf342c65aeb604f17f7e056b66
josephbrunet <jo.brunet73@gmail.com>
2025-05-27 14:32:12 +0200

    add check if sigma superior to rho

--- src/cardiotensor/orientation/orientation_computation_pipeline.py
+++ src/cardiotensor/orientation/orientation_computation_pipeline.py
@@ -73,6 +73,11 @@ def compute_orientation(
     N_SLICE_TEST = params.get("N_SLICE_TEST", None)
     USE_GPU = params.get("USE_GPU", use_gpu)
 
+    if SIGMA > RHO:
+        raise ValueError(
+            "SIGMA must be less than or equal to RHO. Please check your configuration file."
+        )    
+            
     print("\n---------------------------------")
     print("READING VOLUME INFORMATION\n")
     print(f"Volume path: {VOLUME_PATH}")
