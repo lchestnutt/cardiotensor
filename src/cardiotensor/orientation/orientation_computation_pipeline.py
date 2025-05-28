a66783cb5e7175a33dcbdd91cd7033e05ae54571
josephbrunet <jo.brunet73@gmail.com>
2025-05-28 11:54:03 +0200

    improve prints

--- src/cardiotensor/orientation/orientation_computation_pipeline.py
+++ src/cardiotensor/orientation/orientation_computation_pipeline.py
@@ -73,13 +73,30 @@ def compute_orientation(
     N_SLICE_TEST = params.get("N_SLICE_TEST", None)
     USE_GPU = params.get("USE_GPU", use_gpu)
 
+    # Print summary
+    print(f"""
+    ➡️  Key Parameters:
+        - Volume path:    {VOLUME_PATH}
+        - Mask path:      {MASK_PATH or '[None]'}
+        - Output dir:     {OUTPUT_DIR}
+        - Output format:  {OUTPUT_FORMAT}
+        - Output type:    {OUTPUT_TYPE}
+        - Sigma / Rho:    {SIGMA} / {RHO}
+        - Truncate:       {TRUNCATE}
+        - Write angles:   {WRITE_ANGLES}
+        - Write vectors:  {WRITE_VECTORS}
+        - Use GPU:        {USE_GPU}
+    """)  
+    
     if SIGMA > RHO:
         raise ValueError(
             "SIGMA must be less than or equal to RHO. Please check your configuration file."
-        )    
+        )  
             
-    print("\n---------------------------------")
+    print("\n" + "-" * 40)
     print("READING VOLUME INFORMATION\n")
+    print("-" * 40 + "\n")
+    
     print(f"Volume path: {VOLUME_PATH}")
 
     data_reader = DataReader(VOLUME_PATH)
@@ -184,13 +201,17 @@ def compute_orientation(
 
         volume[mask == 0] = 0
 
-    print("\n---------------------------------")
-    print("CALCULATING STRUCTURE TENSOR")
+    print("\n" + "-" * 40)
+    print("CALCULATING STRUCTURE TENSOR\n")
+    print("-" * 40 + "\n")
     t1 = time.perf_counter()  # start time
     val, vec = calculate_structure_tensor(volume, SIGMA, RHO, TRUNCATE=TRUNCATE, use_gpu=USE_GPU)
-    print(f"Vector shape: {vec.shape}")
+    
+    print(f"Vector field shape: {vec.shape}")
 
     if is_mask:
+        print("Applying mask to tensors and vectors...")
+
         volume[mask == 0] = np.nan
         val[0, :, :, :][mask == 0] = np.nan
         val[1, :, :, :][mask == 0] = np.nan
@@ -199,7 +220,7 @@ def compute_orientation(
         vec[1, :, :, :][mask == 0] = np.nan
         vec[2, :, :, :][mask == 0] = np.nan
 
-        print("Mask applied to image volume")
+        print("Masking complete")
 
         del mask
 
@@ -219,7 +240,10 @@ def compute_orientation(
     t2 = time.perf_counter()  # stop time
     print(f"finished calculating structure tensors in {t2 - t1} seconds")
 
-    print("\nCalculating helix/intrusion angle and fractional anisotropy:")
+    print("\n" + "-" * 40)
+    print("ANGLE & ANISOTROPY CALCULATION\n")
+    print("-" * 40 + "\n")
+
     if MULTIPROCESS and not IS_TEST:
         num_slices = vec.shape[1]
         print(f"Using {mp.cpu_count()} CPU cores")
